e29b3423d861d8a8aa425f9151e803d4cdb09fb8
Michael Molisani <molisani@seas.upenn.edu>
2013-09-07 15:54:23 -0400

    utilities.js is now library to access site lists
    Must call getBlocked() before calling blockedSites. Same for
    trackedSites.

--- redirect.js
+++ redirect.js
@@ -1,8 +1,7 @@
 var pattern = /[A-z0-9]+\.(com|edu|org|net|xxx|gov|mil|biz|info|mobi|post|pro|ly|io|im|us)/i;
 
-var testArray = ["facebook.com", "reddit.com", "bing.com"];
-
-var blockedSites = getBlocked();
+/*
+var blockedSites = [];
 
 function getBlocked() {
 	console.log("rebuilding blocked sites array");
@@ -11,29 +10,24 @@ function getBlocked() {
 		blockedSites = callback.websites;
 	});
 }
+*/
 
 function blockSite(requested) {
+	getBlocked();
 	console.log("START: " + requested.url);
 	var req = requested.url.match(pattern)[0];
 	var blocked = false;
 	console.log("CURRENT LOG:" + blockedSites);
 	blockedSites.forEach(function(s){
 		console.log("-looking at: " + s);
-		if (s === req || s == req) {
+		if (s === req) {
 			console.log("FOUND MATCH, WILL BLOCK");
 			blocked = true;
 		}
 	});
-	/*testArray.forEach(function(s){
-		if (s === req) {
-			console.log(s);
-			blocked = true;
-			return;
-		}
-	});*/
 	if (blocked) {
 		console.log("REDIRECTING:" + requested.url);
-			return {redirectUrl: "https://www.google.com"};
+			return {redirectUrl: "chrome-extension://mbipjdgekkbkafemhejdbgaencplgmca/index.html"};
 	} else {
 		console.log("ALLOWING: " + requested.url);
 		return {redirectUrl: requested.url};
