994f1649bb45c8d90ca41e6b0f13812c5d9f8b52
Michael Molisani <molisani@seas.upenn.edu>
2013-09-07 20:28:30 -0400

    CLEANING UP
    Deleting old files

--- formatFix.js
+++
@@ -1,8 +0,0 @@
-var pattern = /[A-z0-9]+\.(com|edu|org|net|xxx|gov|mil|biz|info|mobi|post|pro|ly|io|im|us)/i
-
-	sites.forEach(function(s) {
-		var print = s.match(pattern)[0];
-		s = "*://*." + print + "/*";
-		console.log(s);
-		});
-	
